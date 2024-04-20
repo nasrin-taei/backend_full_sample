acf972f1c17dc30d19b19c3547d21873e8bc29d1
nasrin <nasrin.taei@test.com>
2024-04-20 22:45:18 +0330

    division by zero is invalid

--- service/div.go
+++ service/div.go
@@ -1,9 +1,13 @@
 package service
 
 import (
+	"errors"
 	"restful/domain/service_model"
 )
 
 func DivService(req service_model.DivSvcReq) (service_model.DivSvcRes, error) {
+	if req.Num2 == 0 {
+		return service_model.DivSvcRes{}, errors.New("invalid request data")
+	}
 	return service_model.DivSvcRes{Result: req.Num1 / req.Num2}, nil
 }
